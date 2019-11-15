852e2dc22913688b1462ef078c7856bfbf1f49ef
cdtafud <cdtafud@unal.edu.co>
2019-11-15 02:08:52 -0500

    implementando q&m de location, profile y chat

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 var jwt = require('jsonwebtoken');
 const URL_LOGIN = `http://35.208.241.159:3001`;
 const URL_LOCATIONS = `http://35.208.164.215:3000`;
+const URL_PROFILE = `http://35.208.164.215:3001`;
+const URL_CHAT = `http://35.206.116.17:3001`;
 
 const {APP_SECRET, getEmail} = require('./utils')
 
@@ -19,6 +21,7 @@ const resolvers = {
             else
                 return false;
         },
+        //locations
         allLocations: async(parent, args, context) => {
 
             const userEmail = getEmail(context)
@@ -42,6 +45,28 @@ const resolvers = {
             } else {
                 throw new Error('Unauthorized');
             }
+        },
+        //profile
+        profileByID: async(parent, args, context) => {
+            let res = await axios.get(`${URL_PROFILE}/profile/${args.profile_id}`);
+            console.log(res.data)
+            return res.data;
+        },
+        profileByEmail: async(parent, args, context) => {
+            let res = await axios.get(`${URL_PROFILE}/profile/?email=${args.profile_email}`);
+            console.log(res.data);
+            return res.data;
+        },
+        //chat
+        requestMyRooms: async(parent, args, context) => {
+            let res = await axios.get(`${URL_CHAT}/${args.id}/room`);
+            console.log(res.data);
+            return res.data;
+        },
+        listMessages: async(parent, args, context) => {
+            let res = await axios.get(`${URL_CHAT}/${args.id}`);
+            console.log(res.data[0].messages);
+            return res.data;
         }
     },
 
@@ -65,12 +90,6 @@ const resolvers = {
         },
 
         create: async (parent, args, context) => {
-            //const userEmail = getEmail(context)
-            /*
-            if(userEmail != false)
-                console.log("Esta Autorizado")
-            else
-                console.log("No esta Autorizado")*/
 
             const infoProfile = {
                 nickname: '',
@@ -95,6 +114,58 @@ const resolvers = {
 			}
 			else {
 				throw new Error('User already exists');; }
+        },
+        //location
+        createLocation: async (parent, args, context) => {
+            let res = await axios.post(`${URL_LOCATIONS}/locations`, args.location);
+            console.log(res.data)
+            return res.data;
+        },
+
+        deleteLocation: async (parent, args, context) => {
+            let res = await axios.delete(`${URL_LOCATIONS}/locations/${args.id}`);
+            console.log(res.data)
+            return true;
+        },
+
+        updateLocation: async (parent, args, context) => {
+            let res = await axios.put(`${URL_LOCATIONS}/locations/${args.id}`, args.location)
+            console.log(res.data)
+            return res.data;
+        },
+        //profile
+        addFavToPost: async (parent, args, context) => {
+
+            const favo = {
+                fk_post: args.fav.fk_post
+            }
+
+            let res = await axios.post(`${URL_PROFILE}/favorite/${args.fav.id}`, favo)
+            console.log(res.data)
+            return true;
+        },
+        updateProfile: async (parent, args, context) => {
+
+            let res = await axios.put(`${URL_PROFILE}/profile/${args.id}`, args.profile)
+            //console.log(res)
+            console.log(res.status)
+            if(res.status === 200){
+                return true;
+            }
+            else{
+                return false;
+            }  
+        },
+        deleteProfile: async (parent, args, context) => {
+            let res = await axios.delete(`${URL_PROFILE}/profile/${args.id}`)
+            console.log(res.data)
+            return true;
+        },
+        //chat
+        createRoom: async (parent, args, context) => {
+            let res = await axios.post(`${URL_CHAT}/room`, args)
+            console.log(res.data)
+            return true;
         }
     },
 }
